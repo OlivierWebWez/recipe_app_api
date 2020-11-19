5a8152e42174e87d3413081024eb712f73491292
Olivier <62337923+OlivierWebWez@users.noreply.github.com>
2020-11-19 13:29:34 -0500

    recipe detail feature

---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,102 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from core.models import Recipe, Tag, Ingredient
+
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
+
+
+RECIPES_URL = reverse('recipe:recipe-list')
+
+
+def detail_url(recipe_id):
+    """Return recipe detail URL"""
+    return reverse('recipe:recipe-detail', args=[recipe_id])
+
+
+def sample_tag(user, name='Main Course'):
+    return Tag.objects.create(user=user, name=name)
+
+
+def sample_ingredient(user, name='Main Ingredient'):
+    return Ingredient.objects.create(user=user, name=name)
+
+
+def sample_recipe(user, **params):
+    """creat a recipe and return it"""
+    defaults = {
+        'title': 'Sample Recipe',
+        'time_minute': 5,
+        'price': 5.00
+    }
+    defaults.update(params)
+
+    return Recipe.objects.create(user=user, **defaults)
+
+
+class PublicRecipeApiTests(TestCase):
+    """Test the publicly available tags API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Test that login is required for retrieving recipe"""
+        res = self.client.get(RECIPES_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateRecipeApiTests(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            'oelewi@weebce.ca',
+            'tetetoeie'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_recipes(self):
+        """Test retrieving recipes"""
+        sample_recipe(self.user)
+        sample_recipe(self.user)
+
+        res = self.client.get(RECIPES_URL)
+
+        recipes = Recipe.objects.all().order_by('-id')
+        serializer = RecipeSerializer(recipes, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_recipes_limited_to_user(self):
+        user2 = get_user_model().objects.create_user(
+            'olivdsfier@weebce.ca',
+            'tetetoeie'
+        )
+
+        sample_recipe(user2)
+        sample_recipe(self.user)
+
+        res = self.client.get(RECIPES_URL)
+        recipes = Recipe.objects.filter(user=self.user)
+        serializer = RecipeSerializer(recipes, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_view_recipe_detail(self):
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tag(user=self.user))
+        recipe.ingredients.add(sample_ingredient(user=self.user))
+
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
+
+        self.assertEqual(res.data, serializer.data)
