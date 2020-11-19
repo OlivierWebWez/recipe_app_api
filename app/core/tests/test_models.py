db467650d5cb6bb15a4a26ea7f323d1da1583283
Olivier <62337923+OlivierWebWez@users.noreply.github.com>
2020-11-19 10:38:34 -0500

    add recipe

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -59,3 +59,13 @@ class ModelTests(TestCase):
             name='Cucumber'
         )
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representative"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Test Recipe',
+            time_minute=5,
+            price=5.00
+        )
+        self.assertEqual(str(recipe), recipe.title)
